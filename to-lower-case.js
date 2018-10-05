e9a5c6d064227a31a180f5c824cfb4973457497e
David <david.rud135@gmail.com>
2018-10-05 22:33:33 +0300

    Better solution using RegExp

--- to-lower-case.js
+++ to-lower-case.js
@@ -23,13 +23,7 @@ Output: "here"
  */
 
 let toLowerCase = function(str) {
-  const DIFFERENCE_BTW_UPCASE_AND_LOWCASE_LETTER = 32;
-  let lowerCaseLettersArr = str.split('').map(char => {
-    const letterCode = char.charCodeAt();
-    const upperCaseLetter = letterCode > 64 && letterCode < 91;
-    return (upperCaseLetter) 
-      ? String.fromCharCode(letterCode + DIFFERENCE_BTW_UPCASE_AND_LOWCASE_LETTER)
-      : String.fromCharCode(letterCode);
+  return str.replace(/[A-Z]/g, char => {
+    return String.fromCharCode(char.charCodeAt() + 32);
   });
-  return lowerCaseLettersArr.join('');
 };
